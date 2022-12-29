190913b290b9fec237869591036a1e5e98157f2c
lucoiso <lucas.oliveiravb@gmail.com>
2022-12-28 21:19:23 -0300

    fix indent

--- PassFileConverter/main.cpp
+++ PassFileConverter/main.cpp
@@ -7,13 +7,13 @@
 
 int main()
 {
-    std::unique_ptr<DataLoader> dataLoader = std::make_unique<DataLoader>();    
-    
+    std::unique_ptr<DataLoader> dataLoader = std::make_unique<DataLoader>();
+
     try
     {
         const std::string input_path = Helper::askQuestion("input file path: ");
         dataLoader->loadFile(input_path, LoginDataOrigin::Kapersky);
-        
+
         const std::string output_path = Helper::askQuestion("output file path: ");
         dataLoader->saveFile(output_path, LoginDataOrigin::Google);
 
@@ -23,15 +23,15 @@ int main()
             dataLoader->exportInvalidData(invalid_output_path);
         }
     }
-	catch (const std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 
     dataLoader.reset();
 
-	// 0: No | 1: Yes | Default: Undefined, ask again
-    const auto restart_lambda = [] () -> std::uint8_t
+    // 0: No | 1: Yes | Default: Undefined, ask again
+    const auto restart_lambda = []() -> std::uint8_t
     {
         if (const std::string restart_app = Helper::askQuestion("restart app? (Y/N): ");
             restart_app.contains("y") || restart_app.contains("Y"))
@@ -49,15 +49,15 @@ int main()
     };
 
     std::uint8_t restart_ans = restart_lambda();
-	while (restart_ans != 1 && restart_ans != 0)
-	{
-		restart_ans = restart_lambda();
-	}
-    
+    while (restart_ans != 1 && restart_ans != 0)
+    {
+        restart_ans = restart_lambda();
+    }
+
     if (restart_ans == 1)
     {
         system("cls");
-        
+
         return main();
     }
     else if (restart_ans == 0)
